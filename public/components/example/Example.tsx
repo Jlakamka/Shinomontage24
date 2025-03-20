9970107996e60a664d75c9f5dbe4ad59622e6939
Jlakamka <selov.dmitriy@mail.ru>
2025-03-20 10:54:01 +0300

    fix img

--- public/components/example/Example.tsx
+++ public/components/example/Example.tsx
@@ -1,19 +1,20 @@
 import Image from "next/image";
-import exampleImage1 from "../../image/example/example1.jpg";
-import exampleImage2 from "../../image/example/example2.jpg";
-import exampleImage3 from "../../image/example/example3.jpg";
-import exampleImage4 from "../../image/example/example4.jpg";
-import exampleImage5 from "../../image/example/example5.jpg";
-import exampleImage6 from "../../image/example/example1.jpg";
+// import exampleImage1 from "../../image/example/example1.jpg";
+// import exampleImage2 from "../../image/example/example2.jpg";
+// import exampleImage3 from "../../image/example/example3.jpg";
+// import exampleImage4 from "../../image/example/example4.jpg";
+// import exampleImage5 from "../../image/example/example5.jpg";
+// import exampleImage6 from "../../image/example/example1.jpg";
+import exampleImage from "../../image/image_block.webp";
 import "./example.css";
 export default function Example() {
   const exampleDate = [
-    exampleImage1,
-    exampleImage2,
-    exampleImage3,
-    exampleImage4,
-    exampleImage5,
-    exampleImage6,
+    exampleImage,
+    exampleImage,
+    exampleImage,
+    exampleImage,
+    exampleImage,
+    exampleImage,
   ];
   return (
     <section className="container example">

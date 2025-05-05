48fbeb9ebd44752a7918cd1cc900e7a4cfffb2c9
Jlakamka <selov.dmitriy@mail.ru>
2025-05-05 23:12:21 +0300

    add addaptiv

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Шиномонтаж",
-  description: "Шиномонтаж",
+  description: "Шиномонтаж в Ярославле",
 };
 
 export default function RootLayout({

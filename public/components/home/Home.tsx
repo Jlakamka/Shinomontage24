88185cf1c4efecf9a9db3000e878dbfb8330e98b
Jlakamka <selov.dmitriy@mail.ru>
2025-05-26 18:32:46 +0300

    update home img

--- public/components/home/Home.tsx
+++ public/components/home/Home.tsx
@@ -1,11 +1,12 @@
 import "./home.css";
 import Image from "next/image";
 import startImg from "../../image/startPage.jpg";
+import startImg2 from "../../image/homePageImg.jpg";
 
 export default function Home() {
   return (
     <section className="home container">
-      <Image className="home__img" src={startImg} alt="startImg" />
+      <Image className="home__img" src={startImg2} alt="startImg" />
     </section>
   );
 }
